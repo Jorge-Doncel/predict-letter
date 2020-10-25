7396bb7f3a202c163b1e2dd71dc20ea2bfa26814
jorge doncel <jdoncel27@gmail.com>
2020-10-25 12:57:32 +0100

    streamlit

---
+++ app/first_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+
+
+"""
+# Predict letter
+Here's our first attempt at using data to create a table:
+"""
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)

bcfc4be67140e8a261f2d508e893ad56ba297ccc
jorge doncel <jdoncel27@gmail.com>
2020-11-05 13:51:32 +0100

    New features

--- app/app.py
+++ app/app.py
@@ -4,8 +4,9 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
+import re
 sns.set()
-from fun import next_letter, next_vowel
+from fun import next_letter, next_vowel, clean_data_letter, clean_data_vowel, empty_panel
 
 st.title("Predict next letter")
 st.subheader("Select the letter/s you have and predict the next one")
@@ -17,8 +18,11 @@ data_select = st.selectbox('Select your language',filenames )
 
 st.info(f'You selected {data_select}')
 
+lan= re.sub(r'.csv', '', data_select)
+
 #Read Data
 df= pd.read_csv(f'../Files/{data_select}', index_col=0)
+df_AllLetters= pd.read_csv(f'../Data_files/{lan}_allwords.csv', index_col=0)
 
 #Show Dataset      #PONER PORCENTAJE DE VECES QUE APARECE CADA COLUMNA Y ELEGIR QUE DATASET SE QIERE VER
 
@@ -29,15 +33,23 @@ if st.checkbox("Show Dataset"):
 
 all_letters= df.columns.tolist()
 
-letters= st.multiselect("Select letters you have", all_letters)
+question1 = st.radio("What do you have?", ('Letter/s','Empty panel'))
 
-question = st.radio("What do you need?", ('Consonant', 'Vowel'))
+if question1 == 'Letter/s':
+  letters= st.multiselect("Select letters you have", all_letters)
+  question2 = st.radio("What do you need?", ('Consonant', 'Vowel'))
+  if question2 == "Consonant":
+    st.subheader(next_letter(df, letters))
+    st.dataframe(clean_data_letter(df, letters))
 
-if question == "Consonant":
-  st.subheader(next_letter(df, letters))
+  if question2 == "Vowel":
+    st.subheader(next_vowel(df, letters))
+    st.dataframe(clean_data_vowel(df, letters))
 
-if question == "Vowel":
-  st.subheader(next_vowel(df, letters))
+if question1 == 'Empty panel':
+  st.subheader(empty_panel(df_AllLetters))
+  st.dataframe(df_AllLetters)
+
 
 
 
--- app/fun.py
+++ app/fun.py
@@ -52,3 +52,49 @@ def next_vowel(df,*vowel):
         return f'You should choose {final_vowel} with {df_vowel.loc[final_vowel].per.round(3)}% appear or {df_vowel.iloc[1].name} with {df_vowel.loc[df_vowel.iloc[1].name].per.round(3)}% appear'
     else:
         return f'You should choose {final_vowel} with a {df_vowel.loc[final_vowel].per.round(3)}% appear'
+
+def clean_data_letter(df, *letter):
+    #Create df with letters as an index
+    df_words= df.loc[letter]
+    #Get new row with total letters appears
+    total = df_words.sum()
+    total.name = 'total'
+    # Assign sum of all rows of DataFrame as a new Row
+    df_words = df_words.append(total.transpose())
+    vowels=["a","e", "i", "o", "u"]
+    #Delete vowels and letters from columns and rows
+    remove= list(set(vowels+list(*letter)))
+    df_words.drop(*letter, axis=0, inplace=True)
+    df_words.drop(remove, axis=1, inplace=True)
+    #New column with percentajes for each vowel
+    df_words= df_words.T[["total"]]
+    df_words['per'] = (df_words.total * 100 / df_words.total.sum()).round(4).astype(str) + '%'
+    df_words.sort_values(by="total", ascending=False, inplace= True)
+    
+    #Get recommended letter
+    final_word= df_words.total.idxmax()
+    return df_words
+
+def clean_data_vowel(df, *vowel):
+    
+    #Create df with letters as an index
+    df_vowel= df.loc[vowel]
+    
+    #Get new row with total letters appears    
+    total = df_vowel.sum()
+    total.name = 'total'
+    # Assign sum of all rows of DataFrame as a new Row
+    vowels=["a","e", "i", "o", "u"]
+    vowels= [i for i in vowels if i not in list(*vowel)]
+    df_vowel = df_vowel.append(total.transpose())
+    df_vowel = df_vowel[[*vowels]].T[["total"]]
+    #New column with percentajes for each vowel
+    df_vowel['per'] = (df_vowel.total * 100 / df_vowel.total.sum()).round(4).astype(str) + '%'
+    df_vowel.sort_values(by="total", ascending=False, inplace= True)
+    
+    #Get recommended vowel
+    final_vowel= df_vowel.total.idxmax()
+    return df_vowel
+
+def empty_panel(df):
+    return f'You should choose {df.iloc[0].name}, {df.iloc[1].name} , {df.iloc[2].name}, {df.iloc[3].name} or  {df.iloc[4].name}'
